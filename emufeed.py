fa00159a9570c3ed1828c153303aa34612398d11
eggy <eggyrules@gmail.com>
2022-08-14 15:28:42 -0400

    fix: add check for failed gets

--- emufeed.py
+++ emufeed.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
-from typing import Final
 import iohandler
 import sources
 
-log: Final = iohandler.Logger()
-config: Final = iohandler.Config(log)
+log = iohandler.Logger()
+config = iohandler.Config(log)
 
 
-def main():
+def main() -> None:
     log.info("Reading configuration")
     config.read()
     log.set_debug_level(config.suppress_output, config.verbose)
 
     log.debug("Reading data")
-    data: Final = iohandler.Data(log)
+    data = iohandler.Data(log)
     data.read()
 
-    discord: Final = iohandler.Discord(log, config.webhook_url)
+    discord = iohandler.Discord(log, config.webhook_url)
 
     # import modules
-    source_modules: Final[list] = []
+    source_modules = []
     for c in config.sources:
         if hasattr(sources, c):
             source_modules.append(getattr(sources, c)(log, config))
@@ -30,15 +29,21 @@ def main():
     # check for updates
     for source in source_modules:
         log.debug(f"Getting data for module {source.name}")
-        title, description, link = source.get_latest()
+
+        try:
+            title, description, link = source.get_latest()
+        except IndexError:
+            # error
+            log.error(f"Source {source.name} failed to return proper data")
+            continue
 
         # if this is first run for module and no change
-        module_name: Final[str] = type(source).__name__
+        module_name = type(source).__name__
         if module_name in data.data and data.data[module_name] == link:
             log.debug(f"No updates found for module {module_name}")
         elif module_name not in data.data and config.ignore_if_empty_json:
             log.debug(
-                f"First run detected and SendLatestOnFirstRun disabled, ignoring")
+                "First run detected and SendLatestOnFirstRun disabled, ignoring")
         else:
             discord.send(source, title, description, link)
         data.data[module_name] = link
